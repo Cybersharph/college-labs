cd284f8e48da2b3dcb817f2773f9695f613596d3
Muyivu Shafiq <cybersharph@gmail.com>
2023-09-16 18:32:14 +0300

    Updated CopyArrayItems.java

--- Practice Lab 2/src/CopyArrayItems.java
+++ Practice Lab 2/src/CopyArrayItems.java
@@ -2,28 +2,57 @@
 import java.util.Arrays;
 
 public class CopyArrayItems {
-	
-	public static void main(String[] args) {
-		
-		int[] list = {1, 2, 3};
-		
-		/** 
-		 * Copy "list" Array to "newList Array
-		 * Has same length as original	Array
-		 */
-		
-		int[] newList = Arrays.copyOf(list, list.length);
-		
-//		Arrays.fill(newList, 0); // Fill an array with a single value;
-		
-		for (int i = 0; i < newList.length; i++) {
-			System.out.print(newList[i]);
-		}
-		
-		System.out.println("");
-		
-//		Binary search
-		int three = Arrays.binarySearch(newList, 1);
-		System.out.println(three);
-	}
+  
+  public static void main(String[] args) {
+    
+    int[] list = {1, 2, 3};
+    
+    /** 
+     * Copy "list" Array to "newList Array
+     * Has same length as original  Array
+     */
+    
+    int[] newList = Arrays.copyOf(list, list.length);
+    
+//    Arrays.fill(newList, 0); // Fill an array with a single value;
+    
+    for (int i = 0; i < newList.length; i++) {
+      System.out.print(newList[i]);
+    }
+    
+    System.out.println("");
+    
+//    Binary search | list must be sorted.
+    int three = Arrays.binarySearch(newList, 1);
+    System.out.println(three);
+  }
+}
+
+import java.util.Arrays;
+
+public class CopyArrayItems {
+  
+  public static void main(String[] args) {
+    
+    int[] list = {1, 2, 3};
+    
+    /** 
+     * Copy "list" Array to "newList Array
+     * Has same length as original  Array
+     */
+    
+    int[] newList = Arrays.copyOf(list, list.length);
+    
+//    Arrays.fill(newList, 0); // Fill an array with a single value;
+    
+    for (int i = 0; i < newList.length; i++) {
+      System.out.print(newList[i]);
+    }
+    
+    System.out.println("");
+    
+//    Binary search | list must be sorted. 123
+    int three = Arrays.binarySearch(newList, 1);
+    System.out.println(three);
+  }
 }
