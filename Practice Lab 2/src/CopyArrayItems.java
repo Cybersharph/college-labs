0feab3a0c1950a26b20668e834981c4e31362a37
Muyivu Shafiq <cybersharph@gmail.com>
2023-09-16 10:28:59 +0300

    Updated 'CopyArrayItems.java'

--- Practice Lab 2/src/CopyArrayItems.java
+++ Practice Lab 2/src/CopyArrayItems.java
@@ -1,15 +1,29 @@
+
 import java.util.Arrays;
 
 public class CopyArrayItems {
 	
-
 	public static void main(String[] args) {
-		int[] list = {3, 2, 1, 2};
 		
-		int[] newList = Arrays.copyOf(list, 3);
+		int[] list = {1, 2, 3};
+		
+		/** 
+		 * Copy "list" Array to "newList Array
+		 * Has same length as original	Array
+		 */
+		
+		int[] newList = Arrays.copyOf(list, list.length);
+		
+//		Arrays.fill(newList, 0); // Fill an array with a single value;
 		
 		for (int i = 0; i < newList.length; i++) {
 			System.out.print(newList[i]);
 		}
+		
+		System.out.println("");
+		
+//		Binary search
+		int three = Arrays.binarySearch(newList, 1);
+		System.out.println(three);
 	}
 }
