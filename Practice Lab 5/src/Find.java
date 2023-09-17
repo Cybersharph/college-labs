686f6f30f92b1682fb8ff290d752cf5150e01dd3
Muyivu Shafiq <cybersharph@gmail.com>
2023-09-17 03:02:25 +0300

    VS Code added .project to all Labs, Updated Find.java

--- Practice Lab 5/src/Find.java
+++ Practice Lab 5/src/Find.java
@@ -11,6 +11,8 @@ public class Find {
 	 */
 	public static String find(int[] A, int N) {
 		for (int index = 0; index < A.length; index++) {
+			
+			// Check whether we've found a number at any index
 			if (N == A[index]) {
 				return "Number is at index: " + Integer.toString(index);
 			}
--- REAME.md
+++
@@ -1,6 +0,0 @@
-# My College Labs
-
-## This is a Collection of my University of the People Programming Assigments and Code Labs.
-
-
-## Author: Muyivu Shafiq | Dep't of Computer Science
