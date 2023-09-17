cf9b1f2a94b55c9d8ea2eb172b48d6cfbe4074b8
Muyivu Shafiq <cybersharph@gmail.com>
2023-09-18 00:24:31 +0300

    Updated Practice Lab 6 Implemented Binary Search Algorith

--- Practice Lab 6/src/BinarySearch.java
+++ Practice Lab 6/src/BinarySearch.java
@@ -1,9 +1,12 @@
 
 public class BinarySearch {
-
+	
 	public static void main(String[] args) {
-		// Soon coding
-
+		
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+		System.out.print("Enter number to Search in DB: ");
+		int number = TextIO.getInt();
+		System.out.println(Find.find(numbers, number));
+		
 	}
-
 }
--- Practice Lab 6/src/Find.java
+++ Practice Lab 6/src/Find.java
@@ -1,4 +1,30 @@
-
 public class Find {
-
+	/**
+	 * method implementing the Search
+	 * @param numbers array of numbers to be searched
+	 * @param number an element to be search
+	 * @return index of the number in an array
+	 * @author Muyivu Shafiq
+	 */
+	public static int find(int[] numbers, int number) {
+		
+		int lowestIndex = 0;
+		int highestIndex = numbers.length - 1;
+		
+		while(lowestIndex <= highestIndex) {
+			int middleIndex = (lowestIndex + highestIndex) / 2;
+			int middleNumber = numbers[middleIndex];
+			
+			if  (number == middleNumber) {
+				return middleIndex;
+			} else if (number < middleNumber){
+				highestIndex = middleIndex - 1;
+			} else {
+				lowestIndex = middleIndex + 1;
+			}
+		}
+		
+		// in case no value
+		return -1;
+	}
 }
